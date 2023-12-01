b37af89ad44b2b91c5d8831d8ae91fd5355a1436
Nick Tchayka <nick@booster.cloud>
2023-12-01 16:36:33 +0000

    aider: Expose `todo` in `core/src/Core.hs`.
    # Aider chat conversation:
    
    USER: Expose `todo` in `core/src/Core.hs`
    
    ASSISTANT: To expose `todo` in `core/src/Core.hs`, we need to add it to the export list of the `Core` module. Here is the *SEARCH/REPLACE* block for the `core/src/Core.hs` file:
    
    core/src/Core.hs
    ```haskell
    <<<<<<< SEARCH
    module Core (
      module Reexported,
    ) where
    
    import Console as Reexported
    import HaskellCompatibility.IO as Reexported (MainFunction)
    import Types as Reexported
    =======
    module Core (
      module Reexported,
      todo,
    ) where
    
    import Console as Reexported
    import Debug as Reexported (todo)
    import HaskellCompatibility.IO as Reexported (MainFunction)
    import Types as Reexported
    >>>>>>> REPLACE
    ```
    
    This change adds `todo` to the export list of the `Core` module and ensures that it is imported from the `Debug` module.

--- core/src/Core.hs
+++ core/src/Core.hs
@@ -3,8 +3,10 @@
 -- This module is automatically imported in all the NeoHaskell files,
 module Core (
   module Reexported,
+  todo,
 ) where
 
 import Console as Reexported
+import Debug as Reexported (todo)
 import HaskellCompatibility.IO as Reexported (MainFunction)
 import Types as Reexported
