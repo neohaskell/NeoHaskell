78d561607cdec808f454d3316fa09fb7a396b301
Nick Tchayka <nick@booster.cloud>
2023-12-01 17:02:35 +0000

    aider: Add `Record` module to the export list of `Core` module in `core/src/Core.hs`.
    # Aider chat conversation:
    
    ASSISTANT: To reexport the `Record` module from `core/src/Core.hs`, we need to add it to the export list of the `Core` module. Here is the *SEARCH/REPLACE block* for the `core/src/Core.hs` file:
    
    core/src/Core.hs
    ```haskell
    <<<<<<< SEARCH
    module Core (
      module Reexported,
    ) where
    
    import Console as Reexported
    import Debug as Reexported (todo)
    import HaskellCompatibility.IO as Reexported (MainFunction)
    import Types as Reexported
    =======
    module Core (
      module Reexported,
      module Record,
    ) where
    
    import Console as Reexported
    import Debug as Reexported (todo)
    import HaskellCompatibility.IO as Reexported (MainFunction)
    import Types as Reexported
    import Record
    >>>>>>> REPLACE
    ```

--- core/src/Core.hs
+++ core/src/Core.hs
@@ -3,9 +3,11 @@
 -- This module is automatically imported in all the NeoHaskell files,
 module Core (
   module Reexported,
+  module Record,
 ) where
 
 import Console as Reexported
 import Debug as Reexported (todo)
 import HaskellCompatibility.IO as Reexported (MainFunction)
 import Types as Reexported
+import Record
