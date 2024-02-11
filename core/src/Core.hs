2722155706275c3076119b9720dfc48c29b33120
Nick Tchayka <nick@booster.cloud>
2024-02-11 12:21:47 +0000

    Fix

--- core/src/Core.hs
+++ core/src/Core.hs
@@ -11,15 +11,10 @@ import Console as Reexported
 import Debug as Reexported (todo)
 import Default as Reexported
 import HaskellCompatibility.IO as Reexported (MainFunction)
-<<<<<<< HEAD
 import HaskellCompatibility.Syntax as Reexported
-=======
 import HaskellCompatibility.Generic as Reexported (Generic(..))
 import Types as Reexported
 import Kinds as Reexported
-import Record
->>>>>>> e20f7b4 (Add kinds and generic)
 import Operators
 import Record
 import Traits.Addable as Reexported
-import Types as Reexported
