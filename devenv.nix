ceb16a41aeafca8b2ac7a99b07143199bb5b78cf
Nikita Tchayka <nikitatchayka@gmail.com>
2024-03-23 23:19:04 +0000

    Add devenv

---
+++ devenv.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+  languages.haskell.enable = true;
+  languages.haskell.package = pkgs.haskell.compiler.ghc92;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
