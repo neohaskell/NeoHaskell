d817538671226d50972ad7be9b573834d0499d4b
Nikita Tchayka <nick@booster.cloud>
2024-07-15 17:21:42 +0100

    Update

--- cli/src/Neo.hs
+++ cli/src/Neo.hs
@@ -20,17 +20,49 @@ type ProjectDefinition =
 
 
 data Message
-  = ProjectFileRead ProjectDefinition
+  = ProjectFileAccessed (Result File.Error Text)
+  | ProjectFileParsed ProjectDefinition
   | BuildStarted
+  | BuildFailed FailureReason
+
+
+data FailureReason
+  = NoProjectFile
+  | ProjectFileParseError Text
 
 
 init :: (Model, Command Message)
 init = do
   let emptyModel = ANON {project = Nothing}
   let command =
-        File.read "project.json"
-          |> Command.map ProjectFileRead
+        File.read
+          ANON
+            { path = "project.json",
+              expect = File.expectText ProjectFileAccessed
+            }
   (emptyModel, command)
+
+
+update :: Message -> Model -> (Model, Command Message)
+update message model =
+  case message of
+    ProjectFileAccessed (Ok fileContent) -> do
+      let parsedContent = Yaml.parse fileContent
+      case parsedContent of
+        Ok projectDefinition ->
+          model
+            |> update (ProjectFileParsed projectDefinition)
+        Err _ ->
+          model
+            |> update (BuildFailed ProjectFileParseError fileContent)
+    ProjectFileAccessed (Err _) ->
+      (model, Command.none)
+    ProjectFileParsed projectDefinition ->
+      (model {project = Just projectDefinition}, Command.none)
+    BuildStarted ->
+      (model, Command.none)
+    BuildFailed _ ->
+      (model, Command.none)
 
 
 main :: IO ()
