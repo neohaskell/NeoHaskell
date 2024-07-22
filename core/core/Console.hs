ee48d1c54ab27c38a4b7e8fc6983d605b8dfc5cd
NickSeagull <git@nickseagull.dev>
2024-07-22 17:48:11 +0100

    Add cool locs to print

--- core/core/Console.hs
+++ core/core/Console.hs
@@ -3,23 +3,32 @@ module Console (print, readLine) where
 import Appendable ((++))
 import Basics
 import Data.Text.IO qualified
-import GHC.Stack (HasCallStack, callStack, getCallStack, prettySrcLoc)
+import GHC.Stack qualified as Stack
 import LinkedList qualified
 import Maybe (Maybe (..))
-import Text (Text)
+import Text (Text, fromLinkedList)
 import ToText (toText)
 
-
-print :: (HasCallStack) => Text -> IO Unit
+print :: (Stack.HasCallStack) => Text -> IO Unit
 print text = do
-  let maybeLoc = callStack |> getCallStack |> LinkedList.get 0
+  let maybeLoc =
+        Stack.callStack
+          |> Stack.getCallStack
+          |> LinkedList.get 0
+
   case maybeLoc of
     Just (_, loc) -> do
-      let coolLoc = _
-      let message = "[" ++ (prettySrcLoc loc |> toText) ++ "]: " ++ text
+      let coolLoc =
+            Text.fromLinkedList (Stack.srcLocPackage loc)
+              ++ ":"
+              ++ Text.fromLinkedList (Stack.srcLocFile loc)
+              ++ ":"
+              ++ (Stack.srcLocStartLine loc |> toText)
+              ++ ":"
+              ++ (Stack.srcLocStartCol loc |> toText)
+      let message = "[" ++ (coolLoc) ++ "]: " ++ text
       Data.Text.IO.putStrLn message
     _ -> pure ()
-
 
 readLine :: IO Text
 readLine = Data.Text.IO.getLine
