e0115265b5fb1e3a40e6eff48a36052e00c477ad
Nick Seagull <git@nickseagull.dev>
2024-08-13 18:14:52 +0100

    wip

--- cli/src/Neo.hs
+++ cli/src/Neo.hs
@@ -1,116 +1,41 @@
-{-# OPTIONS_GHC -fplugin=Data.Record.Anon.Plugin #-}
-
 module Neo (main) where
 
 import Action qualified
-import Array qualified
 import Core
-import File qualified
-import Result qualified
-import Service qualified
-import ToText (Show)
-import Time qualified
-import Yaml qualified
 
 
-type Model =
-  Record
-    '[ "project" := Maybe ProjectDefinition,
-       "path" := Maybe Path,
-       "count" := Int,
-       "status" := Text
-     ]
-
-
-type ProjectDefinition =
-  Record
-    '[ "name" := Text,
-       "version" := Version
-     ]
+type State =
+  Record '[]
 
 
 data Event
-  = ProjectFileRead Text
-  | ProjectFileAccessErrored File.Error
-  | ProjectFileParsed ProjectDefinition
-  | BuildStarted
-  | Tick
-  | BuildFailed FailureReason
-  deriving (Show)
+  = Transpile TranspileEvent
+  | NoOp
+  deriving (Show, Eq, Ord)
 
 
-data FailureReason
-  = ProjectFileParseError Text
-  deriving (Show)
+type TranspileEvent =
+  Record
+    '[ "inputPath" := Path,
+       "outputPath" := Path
+     ]
 
 
-init :: (Model, Action Event)
+init :: Record '["next" := State, "action" := Action Event]
 init = do
-  let emptyModel =
-        ANON
-          { project = Nothing,
-            path = Nothing,
-            count = 0,
-            status = "Starting up"
-          }
-  let action =
-        File.readText
-          ANON
-            { path = [path|project.yaml|],
-              onSuccess = ProjectFileRead,
-              onError = ProjectFileAccessErrored
-            }
-  (emptyModel, action)
+  let emptyState = ANON {}
+  let action = Action.none
+  ANON {next = emptyState, action = action}
 
 
-update :: Event -> Model -> (Model, Action Event)
-update event model =
+update :: Event -> State -> (State, Action Event)
+update event state =
   case event of
-    ProjectFileRead fileContent -> do
-      let parsedContent = Yaml.parse fileContent
-      let newModel = model {status = "Parsing project file"}
-      case parsedContent of
-        Result.Ok projectDefinition ->
-          (newModel, Action.continueWith (ProjectFileParsed projectDefinition))
-        Result.Err _ -> do
-          let error = ProjectFileParseError fileContent
-          (newModel, Action.continueWith (BuildFailed error))
-    ProjectFileAccessErrored _ ->
-      (model {status = "File Access Errored"}, Action.none)
-    ProjectFileParsed projectDefinition ->
-      (model {project = Just projectDefinition}, Action.none)
-    BuildStarted ->
-      (model {status = "Build Started!"}, Action.none)
-    BuildFailed _ ->
-      (model {status = "Build Failed!"}, Action.none)
-    Tick ->
-      ( model
-          { count = model.count + 1,
-            status = "Count: " ++ toText model.count
-          },
-        Action.none
-      )
-
-
-view :: Model -> Text
-view m =
-  case m.project of
-    Just project ->
-      m.status ++ "\n\n" ++ toText project
-    Nothing ->
-      m.status
-
-
-main :: IO ()
-main =
-  Service.init
-    ( ANON
-        { init = (init),
-          view = (view),
-          triggers =
-            Array.fromLinkedList
-              [ Time.triggerEveryMilliseconds 1000 (\_ -> Tick)
-              ],
-          update = (update)
-        }
-    )
+    Transpile transpileEvent -> do
+      let newState = state
+      let action = Action.none
+      (newState, action)
+    NoOp -> do
+      let newState = state
+      let action = Action.none
+      (newState, action)
--- core/options-parser/Command.hs
+++ core/options-parser/Command.hs
@@ -1,4 +1,4 @@
-module OptionsParser (
+module Command(
   OptionsParser,
   text,
   parseWith,
@@ -17,28 +17,39 @@ import Control.Applicative qualified as Applicative
 import Data.Aeson qualified as Json
 import Data.Either qualified as GHC
 import Data.Functor qualified as Functor
+import Data.Version (Version)
 import Default (Default, defaultValue)
 import LinkedList (LinkedList)
 import Maybe (Maybe (..))
+import Maybe qualified
 import OptEnvConf qualified
 import Record qualified
 import Result (Result (..))
 import Text (Text, fromLinkedList, toLinkedList)
 import ToText (ToText)
-import Version qualified
+import Version (version)
 
 
 newtype OptionsParser value = OptionsParser (OptEnvConf.Parser value)
   deriving (Functor.Functor, Applicative.Applicative)
 
 
-run :: OptionsParser value -> IO value
-run (OptionsParser parser) = do
-  let programDescription = "FNORD"
-  let versionText = "0.0.0"
-  let ver = case Version.parse versionText of
-        Just v -> v
-        Nothing -> dieWith ("Could not parse version " ++ versionText)
+type CommandOptions value =
+  Record
+    [ "name" := Text,
+      "description" := Text,
+      "version" := Maybe Version,
+      "decoder" := OptionsParser value
+    ]
+
+
+run :: CommandOptions value -> IO value
+run options = do
+  let (OptionsParser parser) = options.decoder
+  let programDescription = options.description |> Text.toLinkedList
+  let ver =
+        options.version
+          |> Maybe.withDefault [version|0.0.0|]
   OptEnvConf.runParser ver programDescription parser
 
 
@@ -182,14 +193,6 @@ parseWith parseFunc config = do
 resultToEither :: Result Text value -> GHC.Either (LinkedList Char) value
 resultToEither (Ok val) = GHC.Right val
 resultToEither (Err err) = GHC.Left (Text.toLinkedList err)
-
-
-type CommandOptions value =
-  Record
-    [ "name" := Text,
-      "description" := Text,
-      "decoder" := OptionsParser value
-    ]
 
 
 commands :: Array (CommandOptions value) -> OptionsParser value
