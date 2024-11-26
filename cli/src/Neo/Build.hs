9dca7133a033b92ef0ee0ba03999e5256b307cea
Nikita Tchayka <github@nickseagull.dev>
2024-11-26 17:27:24 +0000

    update

--- cli/src/Neo/Build.hs
+++ cli/src/Neo/Build.hs
@@ -1,7 +1,8 @@
-module Neo.Build (
-    handle,
+module Neo.Build
+  ( handle,
     Error (..),
-) where
+  )
+where
 
 import Array qualified
 import File qualified
@@ -11,39 +12,36 @@ import Neo.Core
 import Subprocess qualified
 import Task qualified
 
-
 data Error
-    = NixFileError
-    | CabalFileError
-    | Error Text
-    deriving (Show)
-
+  = NixFileError
+  | CabalFileError
+  | Error Text
+  deriving (Show)
 
 handle :: ProjectConfiguration -> Task Error ()
 handle config = do
-    let rootFolder = [path|.|]
-    let nixFileName = [path|default.nix|]
-    let cabalFileName = [path|example.cabal|]
+  let rootFolder = [path|.|]
+  let nixFileName = [path|default.nix|]
+  let cabalFileName = [path|example.cabal|]
 
-    let nixFile = Nix.template config
-    let cabalFile = Cabal.template config
+  let nixFile = Nix.template config
+  let cabalFile = Cabal.template config
 
-    File.writeText nixFileName nixFile
-        |> Task.mapError (\_ -> NixFileError)
+  File.writeText nixFileName nixFile
+    |> Task.mapError (\_ -> NixFileError)
 
-    File.writeText cabalFileName cabalFile
-        |> Task.mapError (\_ -> CabalFileError)
+  File.writeText cabalFileName cabalFile
+    |> Task.mapError (\_ -> CabalFileError)
 
-    completion <- Subprocess.open "nix-build" (Array.fromLinkedList []) rootFolder
-    -- completion <- Subprocess.open "nix-build" (Array.fromLinkedList ["-E", nixFile]) rootFolder
-    if completion.exitCode != 0
-        then errorOut completion.stderr
-        else print completion.stdout
-
+  completion <- Subprocess.open "nix-build" (Array.fromLinkedList []) rootFolder
+  -- completion <- Subprocess.open "nix-build" (Array.fromLinkedList ["-E", nixFile]) rootFolder
+  if completion.exitCode != 0
+    then errorOut completion.stderr
+    else print completion.stdout
 
 errorOut :: Text -> Task Error _
 errorOut err =
-    [fmt|Oops the build failed:
+  [fmt|Oops the build failed:
     {err}|]
-        |> Error
-        |> Task.throw
+    |> Error
+    |> Task.throw
