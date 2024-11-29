e51c11cc118ac3a9944a689b24091c9f4cefd774
Nikita Tchayka <github@nickseagull.dev>
2024-11-29 13:13:38 +0000

    Remove devenv

--- core/default.nix
+++ core/default.nix
@@ -1,12 +1,6 @@
-let
-  pkgs = import (builtins.fetchTarball {
-    name = "haskell-fixes";
-    url = "https://github.com/nixos/nixpkgs/archive/c95b3e3904d1c3138cafab0ddfbc08336128f664.tar.gz";
-    sha256 = "03b5i7almr4v68b677qqnbyvrmqdxq02gks7q1jr6kfm2j51bgw5";
-  }) {};
-in
-  pkgs.haskellPackages.developPackage {
-    root = ./.;
-    source-overrides = {
-    };
-  }
+{pkgs ? import ../nix/nixpkgs.nix {} }:
+pkgs.haskellPackages.developPackage {
+  root = ./.;
+  source-overrides = {
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import ./nix/nixpkgs.nix { }  }:
+pkgs.mkShell rec {
+  buildInputs = [ 
+    # Haskell dev tools
+    pkgs.ghc
+    pkgs.haskell-language-server
+    pkgs.ormolu
+    pkgs.hlint
+    pkgs.haskellPackages.zlib
+
+    # Nix dev tools
+    pkgs.nil
+    pkgs.nixfmt-classic
+
+    # Required native libs
+    pkgs.pkg-config
+    pkgs.zlib
+  ];
+
+  # Required for cabal to find the location of zlib and other native libraries
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+}
