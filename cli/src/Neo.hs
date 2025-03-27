e3952cf88a40c0530da58ebeda394d2403ba2c13
Nick Seagull <git@nickseagull.dev>
2025-03-27 16:51:56 +0000

    chore: Bump version

--- cli/src/Neo.hs
+++ cli/src/Neo.hs
@@ -47,7 +47,7 @@ run = do
         Command.CommandOptions
           { name = "neo",
             description = "NeoHaskell's console helper",
-            version = Just [Core.version|0.5.0|],
+            version = Just [Core.version|0.6.0|],
             decoder = commandsParser
           }
   cmd <- Command.parseHandler parser
