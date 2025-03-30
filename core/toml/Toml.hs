4effde137e951e15efb42a5b415d698b216f8492
Davide Walder <58290976+DavideWalder@users.noreply.github.com>
2025-03-30 22:15:28 +0200

    refactor: enable cabal check (#160)
    * refactor: added category
    
    * fix: empty hs-source-dirs
    
    * cabal freeze
    
    * refactor: check in CI
    
    * refactor: hackage doesn't like Werror
    
    * fix: freeze correctly
    
    * fix: formatting
    
    * refactor: make coderabbit happy

---
+++ core/toml/Toml.hs
@@ -0,0 +1,12 @@
+module Toml (
+  Decodable,
+  Encodable,
+) where
+
+
+-- TODO: implement: https://github.com/neohaskell/NeoHaskell/issues/116
+type Decodable value = value
+
+
+-- TODO: implement: https://github.com/neohaskell/NeoHaskell/issues/116
+type Encodable value = value
