5869d0e3534ba8b68a518c59e8faabec84929a57
Nikita Tchayka <github@nickseagull.dev>
2025-06-03 18:28:03 +0100

    Bump

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,6 +1,6 @@
 import (builtins.fetchTarball {
-  name = "haskell-fixes";
+  name = "master";
   url =
     "https://github.com/nixos/nixpkgs/archive/054144e516ca1e3cf5706d5c3427d350eaa107af.tar.gz";
-  sha256 = "1wcpckkd2i9hxf27l80k35kv4s1jag7wikqj2y0kp7dmavqz7kn4";
+  sha256 = "0ixd2mc9kkmr8mz20ah2j7akgwllxsjv94ah895zgi9b9q1h25ka";
 })
