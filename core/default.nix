624ba5a5345649660666a2f4fb088a417489399f
Nikita Tchayka <github@nickseagull.dev>
2025-06-03 17:35:16 +0100

    Fix?

--- core/default.nix
+++ core/default.nix
@@ -3,4 +3,13 @@ pkgs.haskellPackages.developPackage {
   root = ./.;
   source-overrides = { };
   modifier = drv: pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.dontHaddock drv);
+  overrides = self: super: rec {
+    mkDerivation = args: super.mkDerivation (args // {
+      doCheck = false;
+      doHaddock = false;
+      enableLibraryProfiling = false;
+      enableExecutableProfiling = false;
+      jailbreak = true;
+    })
+  };
 }
