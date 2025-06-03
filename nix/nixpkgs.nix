3071e826c675ec4224377d67fec17d65c810dfec
Nikita Tchayka <github@nickseagull.dev>
2025-06-03 20:56:11 +0100

    Update name

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,5 +1,5 @@
 import (builtins.fetchTarball {
-  name = "master";
+  name = "nixpkgs-054144e5";
   url =
     "https://github.com/nixos/nixpkgs/archive/054144e516ca1e3cf5706d5c3427d350eaa107af.tar.gz";
   sha256 = "0ixd2mc9kkmr8mz20ah2j7akgwllxsjv94ah895zgi9b9q1h25ka";
