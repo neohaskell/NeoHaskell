ae0503a13fa51ed8dd4c1a2fa046efd2264f1936
Nikita Tchayka <github@nickseagull.dev>
2025-08-01 11:44:58 +0100

    format

--- nix/project-shell.nix
+++ nix/project-shell.nix
@@ -2,7 +2,8 @@
 { neoJsonPath, neoHaskellSource, srcPath ? null }:
 let
   common = import ./common.nix { inherit pkgs; };
-  project = common.setupProject { inherit neoJsonPath neoHaskellSource srcPath; };
+  project =
+    common.setupProject { inherit neoJsonPath neoHaskellSource srcPath; };
   generatedFiles = common.generateProjectFiles {
     inherit (project) neoConfig srcPathValue modules generators;
   };
@@ -13,39 +14,40 @@ let
     root = generatedFiles;
     returnShellEnv = true;
     source-overrides = common.sourceOverrides neoHaskellSource;
-    modifier = drv: pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [
-      cabal-install
-      haskell-language-server
-      fourmolu
-      hlint
-    ]);
+    modifier = drv:
+      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [
+        cabal-install
+        haskell-language-server
+        fourmolu
+        hlint
+      ]);
   };
 
   # Convert GHC flags to hie.yaml arguments
   hieArguments = [
-    ''-i${project.actualSrcPath}''
-    ''-package-db''
-    ''$PACKAGE_DB''
-    ''-hide-all-packages''
-    ''-package''
-    ''base''
-    ''-package''
-    ''nhcore''
+    "-i${project.actualSrcPath}"
+    "-package-db"
+    "$PACKAGE_DB"
+    "-hide-all-packages"
+    "-package"
+    "base"
+    "-package"
+    "nhcore"
   ] ++ common.ghcFlags;
 
 in devShell.overrideAttrs (oldAttrs: {
   shellHook = (oldAttrs.shellHook or "") + ''
-    # Use the correct package database path
-    PACKAGE_DB="$NIX_GHC_LIBDIR/package.conf.d"
-    
-    # Generate hie.yaml for HLS using direct cradle to avoid polluting user directory
-    cat > ${project.projectDir}/hie.yaml << 'HIEYAML'
-cradle:
-  direct:
-    arguments:
-${pkgs.lib.concatMapStringsSep "\n" (arg: "      - \"${arg}\"") hieArguments}
-HIEYAML
-    
-    echo "NeoHaskell development environment ready!"
+        # Use the correct package database path
+        PACKAGE_DB="$NIX_GHC_LIBDIR/package.conf.d"
+
+        cat > ${project.projectDir}/hie.yaml << 'HIEYAML'
+    cradle:
+      direct:
+        arguments:
+    ${pkgs.lib.concatMapStringsSep "\n" (arg: "      - \"${arg}\"")
+    hieArguments}
+    HIEYAML
+
+        echo "NeoHaskell development environment ready!"
   '';
 })
