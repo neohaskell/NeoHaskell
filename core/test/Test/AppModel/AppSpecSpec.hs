cfe8c36282958fd7c5900979fa375d480d8f1907
Nikita Tchayka <github@nickseagull.dev>
2025-10-16 12:54:51 +0100

    .

---
+++ core/test/Test/AppModel/AppSpecSpec.hs
@@ -0,0 +1,12 @@
+module Test.AppModel.AppSpecSpec where
+
+import Core
+import Test
+
+
+spec :: Spec Unit
+spec =
+  describe "AppSpec" do
+    describe "verify" do
+      it "fails" \_ -> do
+        fail @Text "not implemented"
