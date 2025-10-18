4bca2524346768e99eb77e041da2a7f163ccfaae
Nikita Tchayka <github@nickseagull.dev>
2025-10-18 13:53:00 +0100

    .

--- core/test/Test/AppSpec/SpecificationForSpec.hs
+++ core/test/Test/AppSpec/SpecificationForSpec.hs
@@ -2,6 +2,7 @@ module Test.AppSpec.SpecificationForSpec where
 
 import Core
 import Test
+import Test.AppSpec (ScenarioDef (..))
 import Test.AppSpec.Core (AppSpec (..))
 
 
@@ -16,8 +17,25 @@ spec :: Spec Unit
 spec =
   describe "specification for" do
     it "can build an empty app spec" \_ -> do
-      let emptyAppSpec = AppSpec @TestAppModel
+      let expectedAppSpec = AppSpec @TestAppModel
 
       let actualAppSpec = specificationFor testAppModel noScenarios
 
-      actualAppSpec |> shouldBe emptyAppSpec
+      actualAppSpec |> shouldBe expectedAppSpec
+
+    it "can build an app spec with one scenario" \_ -> do
+      let expectedAppSpec :: AppSpec TestAppModel =
+            AppSpec
+              { scenarios =
+                  Array.fromList
+                    [ ScenarioDef
+                        { name = "test scenario"
+                        }
+                    ]
+              }
+
+      let actualAppSpec =
+            specificationFor testAppModel do
+              scenario "test scenario" emptyScenario
+
+      actualAppSpec |> shouldBe expectedAppSpec
