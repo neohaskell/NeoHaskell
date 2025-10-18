d1152c3a71ca1a1b01743fd469ff155da6448bbc
Nikita Tchayka <github@nickseagull.dev>
2025-10-18 12:54:54 +0100

    .

--- core/test/Test/AppSpec/SpecificationForSpec.hs
+++ core/test/Test/AppSpec/SpecificationForSpec.hs
@@ -2,10 +2,22 @@ module Test.AppSpec.SpecificationForSpec where
 
 import Core
 import Test
+import Test.AppSpec.Core (AppSpec (..))
+
+
+data TestAppModel = TestAppModel
+
+
+testAppModel :: TestAppModel
+testAppModel = TestAppModel
 
 
 spec :: Spec Unit
 spec =
   describe "specification for" do
-    it "can build an empty" \_ -> do
-      fail "not implemented"
+    it "can build an empty app spec" \_ -> do
+      let emptyAppSpec = AppSpec @TestAppModel
+
+      let actualAppSpec = specificationFor testAppModel noScenarios
+
+      actualAppSpec |> shouldBe emptyAppSpec
