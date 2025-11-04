f416b48c0f42adce1a98f19bec149e2a074f07f8
Nikita Tchayka <github@nickseagull.dev>
2025-11-04 12:46:01 +0000

    .

--- core/test/Service/EventStore/PostgresSpec.hs
+++ core/test/Service/EventStore/PostgresSpec.hs
@@ -1,12 +1,14 @@
 module Service.EventStore.PostgresSpec where
 
 import Core
+import Service.EventStore.Postgres qualified as Postgres
+import Task qualified
 import Test
+import Test.Service.EventStore qualified as EventStore
 
 
 spec :: Spec Unit
 spec = do
   describe "PostgresEventStore" do
-    -- let newStore = Postgres.new |> Task.mapError toText
-    -- EventStore.spec newStore
-    pass
+    let newStore = Postgres.new |> Task.mapError toText
+    EventStore.spec newStore
