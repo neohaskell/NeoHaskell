fc90490ef6b86fbcd2f4f542679a4d5bda7a5737
Nikita Tchayka <github@nickseagull.dev>
2025-11-04 15:15:44 +0000

    .

---
+++ core/service/Service/EventStore/Postgres/Internal/Sessions.hs
@@ -0,0 +1,24 @@
+module Service.EventStore.Postgres.Internal.Sessions where
+
+import Core
+import Hasql.Session qualified as Session
+
+
+createEventsTableSession :: Session.Session ()
+createEventsTableSession =
+  Session.sql
+    [fmt|
+            CREATE TABLE Events (
+                EventId UUID NOT NULL,
+                GlobalPosition BIGSERIAL NOT NULL,
+                StreamPosition BIGINT NOT NULL,
+                InlinedStreamId VARCHAR(4000) NOT NULL,
+                Swimlane VARCHAR(255) NOT NULL,
+                EventType VARCHAR(255) NOT NULL,
+                EventData BYTEA NOT NULL,
+                Metadata BYTEA NULL,
+                CONSTRAINT PK_Events PRIMARY KEY (GlobalPosition),
+                CONSTRAINT UK_Events_EventId UNIQUE (EventId),
+                CONSTRAINT UK_Events_Stream UNIQUE (Swimlane, InlinedStreamId, StreamPosition)
+            )
+          |]
