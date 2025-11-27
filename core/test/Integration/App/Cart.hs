616187dfb25163d0f00c379e94bce9b10ef04b35
Nikita Tchayka <github@nickseagull.dev>
2025-11-27 13:07:21 +0000

    .

--- core/test/Integration/App/Cart.hs
+++ core/test/Integration/App/Cart.hs
@@ -4,9 +4,8 @@ module Integration.App.Cart (
 
 import Core
 import Integration.App.Cart.Commands.CreateCart (CreateCart)
-import Service.Model.Core qualified as Model
 
 
 model :: Model _ Unit
-model = Model.do
+model = do
   command @CreateCart
