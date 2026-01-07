f1c285774f4c4c1e194af3ae058e71a04c6302b2
Nikita Tchayka <github@nickseagull.dev>
2026-01-07 15:42:02 +0000

    .

--- core/test/Service/CommandHandlerSpec.hs
+++ core/test/Service/CommandHandlerSpec.hs
@@ -1,6 +1,7 @@
 module Service.CommandHandlerSpec where
 
 import Core
+import Service.EventStore.Core qualified as EventStore
 import Service.EventStore.InMemory qualified as InMemory
 import Task qualified
 import Test
@@ -11,5 +12,5 @@ import Test.Service.CommandHandler qualified as CommandHandler
 spec :: Spec Unit
 spec = do
   describe "CommandHandler with InMemory EventStore" do
-    let newStore = InMemory.new @CartEvent |> Task.mapError toText
+    let newStore = InMemory.new |> Task.map (EventStore.castEventStore @CartEvent) |> Task.mapError toText
     CommandHandler.spec newStore
