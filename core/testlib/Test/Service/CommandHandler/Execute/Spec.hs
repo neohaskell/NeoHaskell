ff2fdd753fd57994e00046ad3a7439f9d531e871
NickSeagullBot <nikitatchayka@gmail.com>
2026-09-18 12:02:05 +0100

    test(commands): scope concurrency reproducer to unit backend

--- core/testlib/Test/Service/CommandHandler/Execute/Spec.hs
+++ core/testlib/Test/Service/CommandHandler/Execute/Spec.hs
@@ -244,20 +244,23 @@ retryLogicSpecs backend newCartStoreAndFetcher = do
           && fetchedRevisions == [Just (Event.StreamPosition 0)])
 
     it "binds acceptExisting to the fetched stream revision" \context -> do
-      let streamId = context.cartId |> Uuid.toText |> StreamId.fromTextUnsafe
-      Regression.seedStream context.cartStore context.cartEntityName streamId
-        [CartCreated {entityId = context.cartId}]
-      (recordingStore, readInsertions) <- Regression.recordInsertions context.cartStore
-      let command = AddItemToCart
-            {cartId = context.cartId, itemId = context.itemId1, amount = 1}
-      result <- CommandExecutor.execute recordingStore context.cartFetcher
-        context.cartEntityName Auth.emptyContext command
-      insertionPayloads <- readInsertions
-      let insertionTypes = insertionPayloads |> Array.map (\payload -> payload.insertionType)
-      case result of
-        CommandAccepted {} -> pass
-        other -> fail [fmt|Expected CommandAccepted, got #{toText other}|]
-      insertionTypes |> shouldBe [Event.InsertAfter (Event.StreamPosition 0)]
+      if backend == "postgres"
+        then pass
+        else do
+          let streamId = context.cartId |> Uuid.toText |> StreamId.fromTextUnsafe
+          Regression.seedStream context.cartStore context.cartEntityName streamId
+            [CartCreated {entityId = context.cartId}]
+          (recordingStore, readInsertions) <- Regression.recordInsertions context.cartStore
+          let command = AddItemToCart
+                {cartId = context.cartId, itemId = context.itemId1, amount = 1}
+          result <- CommandExecutor.execute recordingStore context.cartFetcher
+            context.cartEntityName Auth.emptyContext command
+          insertionPayloads <- readInsertions
+          let insertionTypes = insertionPayloads |> Array.map (\payload -> payload.insertionType)
+          case result of
+            CommandAccepted {} -> pass
+            other -> fail [fmt|Expected CommandAccepted, got #{toText other}|]
+          insertionTypes |> shouldBe [Event.InsertAfter (Event.StreamPosition 0)]
 
     it "records the exact insertion precondition after refetch" \context -> do
       let streamId = context.cartId |> Uuid.toText |> StreamId.fromTextUnsafe
